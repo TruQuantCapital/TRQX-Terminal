5821e7957b510b35bc1d504a448b473212d37e95
TruQuantCapital <michaelvalerio0511@gmail.com>
2026-07-19 10:32:11 -0400

    updated auth page

---
+++ src/components/widgets/index.js
@@ -0,0 +1,11 @@
+export { default as WidgetShell } from "./WidgetShell";
+
+export { default as MarketBriefWidget } from "./MarketBriefWidget";
+export { default as EconomicEventsWidget } from "./EconomicEventsWidget";
+export { default as AcademyProgressWidget } from "./AcademyProgressWidget";
+
+import {
+    MarketBriefWidget,
+    EconomicEventsWidget,
+    AcademyProgressWidget,
+} from "../components/widgets";
